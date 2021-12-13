2a075edf31029bd7540fbf3fe250a18ae2556d76
arudy <arudy.student@42.fr>
2021-12-13 14:52:32 +0100

    done

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 11:35:26 by arudy             #+#    #+#             */
-/*   Updated: 2021/12/13 14:23:28 by arudy            ###   ########.fr       */
+/*   Updated: 2021/12/13 14:40:17 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,9 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int		ft_strchr(char *s, char c);
 char	*get_next_line(int fd);
-char	*ft_strcpy(char *dest, char *src);
+int		ft_strchr(char *s, char c);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_trim_stat(char *s);
-char	*ft_error(char *buff, char *stat);
-char	*read_line(int fd, char *stat);
-char	*ft_free(char *s);
 size_t	ft_strlen(char *str);
 
 #endif
